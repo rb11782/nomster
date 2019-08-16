679771ba87abd755f9f5c57410c12b894f9463b9
Raymond Bertrand <rb11782@gmail.com>
2019-08-16 22:30:38 +0000

    I trying to fix usidstatuse.

--- db/migrate/20190722183752_create_places.rb
+++ db/migrate/20190722183752_create_places.rb
@@ -1,6 +1,7 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
+      t.integer :user_id
       t.string :name
       t.text :description
       t.string :address
